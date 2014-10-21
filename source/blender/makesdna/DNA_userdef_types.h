f3d277c280826b82d9c285e6bd3041ed791d05b7
Jonathan Williamson <jonathan@cgcookie.com>
2014-10-21 12:16:12 -0500

    Adjust short pad

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -169,13 +169,12 @@ typedef struct ThemeUI {
 	uiPanelColors panel; /* depricated, but we keep it for do_versions (2.66.1) */
 
 	char emboss[4];
-	char pad3[4];
 
 	/* fac: 0 - 1 for blend factor, width in pixels */
 	float menu_shadow_fac;
 	short menu_shadow_width;
 	
-	short pad;
+	short pad[3];
 	
 	char iconfile[256];	// FILE_MAXFILE length
 	float icon_alpha;
